f06846ccda4ef2257abaf55b07e011574600e5a9
Metehan Arı <metehanari1@gmail.com>
2023-07-30 11:04:09 +0300

    initial

---
+++ IBM_Python_for_Data_Science/1.1.py
@@ -0,0 +1,14 @@
+#some examples about strings.
+name = "Eric Clapton"
+
+#print(name[::2])
+
+#print(name[0:5])
+
+#print(name[5::])
+
+#print(len(name))
+
+#print(name * 2)
+
+#print(name[0])
---
+++ IBM_Python_for_Data_Science/1.2.py
@@ -0,0 +1,25 @@
+#examples about strings and methods
+
+print("Stevie \nWonder")
+print("Stevie \t\t\tWonder")
+
+
+
+# a = "mj is the best"
+# b = a.upper()
+# print(b)
+
+
+
+# a = "mj is the best"
+# b = a.replace("best","worst")
+# print(b)
+
+
+
+
+# name = "Stevie Wonder"
+# a = name.find("vie")
+# print(a)
+
+
